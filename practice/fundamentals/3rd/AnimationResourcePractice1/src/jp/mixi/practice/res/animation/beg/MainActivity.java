dbd837b97547adf3e8151904c7d9c147b0a29063
神林 麻衣 <mai.kambayashi@90201703n.intra.emercury.co.jp>
2013-05-08 10:10:20 +0900

    実習　String Resource1,2,3 Drawable Resource1,2 Animation Resource1, Intent1

--- practice/fundamentals/3rd/AnimationResourcePractice1/src/jp/mixi/practice/res/animation/beg/MainActivity.java
+++ practice/fundamentals/3rd/AnimationResourcePractice1/src/jp/mixi/practice/res/animation/beg/MainActivity.java
@@ -4,6 +4,9 @@ package jp.mixi.practice.res.animation.beg;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 public class MainActivity extends Activity {
 
@@ -11,6 +14,22 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View textView1 = findViewById(R.id.TextView1);
+        View textView2 = findViewById(R.id.TextView2);
+        View textView3 = findViewById(R.id.TextView3);
+        View textView4 = findViewById(R.id.TextView4);
+        View textView5 = findViewById(R.id.TextView5);
+
+        Animation animation1 = AnimationUtils.loadAnimation(this, R.anim.sample_animation1);
+        Animation animation2 = AnimationUtils.loadAnimation(this, R.anim.sample_animation2);
+        Animation animation3 = AnimationUtils.loadAnimation(this, R.anim.sample_animation3);
+        
+        textView1.startAnimation(animation3);
+        textView2.startAnimation(animation1);
+        textView3.startAnimation(animation2);
+        textView4.startAnimation(animation2);
+        textView5.startAnimation(animation3);
     }
 
     @Override
--- practice/fundamentals/3rd/StringResourcePractice2/src/jp/mixi/practice/res/string/med/MainActivity.java
+++ practice/fundamentals/3rd/StringResourcePractice2/src/jp/mixi/practice/res/string/med/MainActivity.java
@@ -46,7 +46,8 @@ public class MainActivity extends Activity implements TextWatcher {
     public void onTextChanged(CharSequence s, int start, int before, int count) {
         TextView countView = (TextView) findViewById(R.id.TextCounter);
         // TODO: フォーマット用の String リソース（text_counter）を取り出し、setText(CharSequence) の引数に渡す
-        countView.setText(null);
+        String text_counter = getString(R.string.text_counter, count, TEXT_MAX_LENGTH);
+        countView.setText(text_counter);
     }
 
     // Show Toast ボタンが押された
@@ -55,6 +56,7 @@ public class MainActivity extends Activity implements TextWatcher {
         String name = nameInput.getText().toString();
         mClickCount++;
         // TODO: フォーマット用の String リソース（toast_message）を取り出し、makeText(Context, CharSequence, int) の第 2 引数に渡す
-        Toast.makeText(this, "", Toast.LENGTH_LONG).show();
+        String toast_message = getString(R.string.toast_message, name, mClickCount);
+        Toast.makeText(this, toast_message, Toast.LENGTH_LONG).show();
     }
 }
